56a99b304582c0fbc9153124095e63b083fe56ae
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-16 14:59:49 +0100

    Testing card data pulls through

--- src/App.js
+++ src/App.js
@@ -6,24 +6,49 @@ function App() {
 	const [loading, setLoading] = useState(true);
 	const [pokemon, setPokemon] = useState([]);
 
-	useEffect(() => {
-		setLoading(true);
+	const getPokemonData = async() => {
+        setLoading(true);
+		const res = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        setPokemonData(res.data.results);
+        setLoading(false)
+    }
 
-		axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
-			setLoading(false);
-			setPokemon(res.data.results);
-		});
+	const setPokemonData = async(res) => {
+        res.map(async(item)=>{
+            const result =  await axios.get(item.url);
+            setPokemon(state => {
+                state = [...state,result.data]
+                state.sort((a,b) => a.id>b.id?1:-1)
+                return state;
+            })
+        })
+    }
+
+	useEffect(() => {
+		getPokemonData();
 	}, []);
 
-  console.log(pokemon);
+	console.log(pokemon);
 
 	if (loading) return "Loading...";
 
-	return (
+	let pokemonId = 3;
+
+	if (pokemon.length > 19) return (
 		<div className="App container">
-			<main className="col-12">
+			<div className="col-12">
 				<h1>Pokemon API App</h1>
-			</main>
+				<main>
+					<div>
+						<small>{pokemon[pokemonId].id}</small>
+						<h2>{pokemon[pokemonId].name}</h2>
+						<div>
+							<p>{pokemon[pokemonId].types[0].type.name}</p>
+						</div>
+						<img src={pokemon[pokemonId].sprites.front_default} alt="" />
+					</div>
+				</main>
+			</div>
 		</div>
 	);
 }
