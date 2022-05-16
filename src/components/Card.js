b70738b558c3683927fed351fd4f4d5e46d9878d
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-16 17:38:15 +0100

    Changed card font colours and also reworked spacings

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Card = ({pokemonArr}) => {
-    console.log(pokemonArr);
+    // console.log(pokemonArr);
     return (
         <>
             <div className="grid">
                 {
                     pokemonArr.map((pokemon) => {
-                        console.log(pokemon.types);
+                        // console.log(pokemon.types);
                         const pokeId = pokemon.id;
                         const pokeName = pokemon.name;
                         const pokeSpirte = pokemon.sprites.front_default;
@@ -15,9 +15,9 @@ const Card = ({pokemonArr}) => {
 
                         return (
                             <div className={`card ${pokeTypes[0].type.name}`} key={pokeId}>
-                                <div className="card__info col-7">
+                                <div className="card__info col-8">
                                     <small className="card__info__id"># {pokeId}</small>
-                                    <h2 className="card__info__name">{pokeName}</h2>
+                                    <h3 className="card__info__name">{pokeName}</h3>
                                     <div className="card__info__types">
                                         {
                                             pokeTypes.map((item) => {
@@ -30,7 +30,7 @@ const Card = ({pokemonArr}) => {
                                         }
                                     </div>
                                 </div>
-                                <div className="card__sprite col-5">
+                                <div className="card__sprite col-4">
                                     <img src={pokeSpirte} alt={pokeName} className="card__sprite__img"/>
                                 </div>
                             </div>
