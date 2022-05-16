6da4ec5ede1d369596a17ff9865c3586b6fd5392
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-16 17:23:59 +0100

    Card styling added inc background image and type colours

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,9 +14,9 @@ const Card = ({pokemonArr}) => {
                         const pokeTypes = pokemon.types;
 
                         return (
-                            <div className="card" key={pokeId}>
+                            <div className={`card ${pokeTypes[0].type.name}`} key={pokeId}>
                                 <div className="card__info col-7">
-                                    <small className="card__info__id">#{pokeId}</small>
+                                    <small className="card__info__id"># {pokeId}</small>
                                     <h2 className="card__info__name">{pokeName}</h2>
                                     <div className="card__info__types">
                                         {
@@ -24,7 +24,7 @@ const Card = ({pokemonArr}) => {
                                                 const pokeType = item.type.name;
 
                                                 return (
-                                                <p className="card__info__types__type">{pokeType}</p> 
+                                                <p className={`card__info__types__type ${pokeType}`}>{pokeType}</p> 
                                                 )
                                             })
                                         }
