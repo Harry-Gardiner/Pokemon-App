d1ab80b4949c511adbd4c480dc05690bf51a19c5
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-17 22:22:31 +0100

    Reworked modal to use Pokemon component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { makeid } from "../functions/functions";
+import PokemonData from "./Pokemon";
 
 const PokemonModal = ({ singlePokemonData, setShowModal }) => {
 	// console.log(singlePokemonData);
@@ -9,41 +10,7 @@ const PokemonModal = ({ singlePokemonData, setShowModal }) => {
 		<div onClick={setShowModal}>
 			<div className="modal-background"></div>
 			<div className="modal">
-				<div className="modal__content">
-					<div className={`column-1 ${p.types[0].type.name}`}>
-						<h1 className="modal__content__name">{p.name}</h1>
-						<img
-							className="modal__content__sprite"
-							src={
-								p.sprites.other.dream_world.front_default !== null
-									? p.sprites.other.dream_world.front_default
-									: p.sprites.front_default
-							}
-							alt={p.name}
-						/>
-					</div>
-					<div className="column-2 modal__content_stats">
-						<p className="modal__content__stats__title">Base Stats</p>
-						{p.stats.map((s) => {
-							const statName = s.stat.name;
-							const statValue = s.base_stat;
-
-							return (
-								<div
-									className="modal__content__stats__container"
-									key={makeid()}
-								>
-									<small className="modal__content__stats__container__stat">
-										{statName}:&nbsp;
-									</small>
-									<small className="modal__content__stats__container__value">
-										{statValue}
-									</small>
-								</div>
-							);
-						})}
-					</div>
-				</div>
+				<PokemonData pokemonObj={p} />
 			</div>
 		</div>
 	);
