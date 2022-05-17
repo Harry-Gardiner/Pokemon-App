0bbc80cd81ecf72d8b070d673dbf6d104e94a2a0
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-17 16:49:19 +0100

    Set up component layout

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const ComparePokemon = () => {
 	return (
-		<div className="comparison">
+		<div className="compare">
 			<h2>Compare Pokemon</h2>
-			<small>Select 2 pokemon to compare by click the pokerball</small>
+			<small>Select 2 pokemon to compare by clicking the pokerball</small>
+			<div className="compare__container row">
+				<div className="column col-5">Test Col 1</div>
+				<div className="column col-2">VS</div>
+				<div className="column col-5">Test Col 2</div>
+			</div>
 		</div>
 	);
 };
