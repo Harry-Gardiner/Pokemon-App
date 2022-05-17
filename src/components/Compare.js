b339b336040f97baa463bfcb0d5700c5ce14523f
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-05-17 22:06:44 +0100

    Created a pokemon component so it can be reused to display a single pokemons details

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -1,77 +1,25 @@
 import React from "react";
 import { makeid } from "../functions/functions";
+import PokemonData from "./Pokemon";
 const ComparePokemon = ({ compare }) => {
 	console.log(compare);
-	let p1 = compare[0];
-	let p2 = compare[1];
+	let pokemon1 = compare[0];
+	let pokemon2 = compare[1];
 	return (
 		<div className="compare">
 			<h2>Compare Pokemon</h2>
 			<small>Select 2 pokemon to compare by clicking the pokerball</small>
 			<div className="compare__container row">
-				<div className={`column col-5 ${p1.types[0].type.name}`}>
-					<h1 className="modal__content__name">{p1.name}</h1>
-					<img
-						className="modal__content__sprite"
-						src={
-							p1.sprites.other.dream_world.front_default !== null
-								? p1.sprites.other.dream_world.front_default
-								: p1.sprites.front_default
-						}
-						alt={p1.name}
-					/>
-
-					<p className="modal__content__stats__title">Base Stats</p>
-					{p1.stats.map((s) => {
-						const statName = s.stat.name;
-						const statValue = s.base_stat;
-
-						return (
-							<div className="modal__content__stats__container" key={makeid()}>
-								<small className="modal__content__stats__container__stat">
-									{statName}:&nbsp;
-								</small>
-								<small className="modal__content__stats__container__value">
-									{statValue}
-								</small>
-							</div>
-						);
-					})}
-				</div>
-				<div className="column col-2">VS</div>
 				<div className="column col-5">
-					<div className={`column col-5 ${p2.types[0].type.name}`}>
-						<h1 className="modal__content__name">{p2.name}</h1>
-						<img
-							className="modal__content__sprite"
-							src={
-								p2.sprites.other.dream_world.front_default !== null
-									? p2.sprites.other.dream_world.front_default
-									: p2.sprites.front_default
-							}
-							alt={p2.name}
-						/>
+					<PokemonData pokemonObj={pokemon1} />
+				</div>
 
-						<p className="modal__content__stats__title">Base Stats</p>
-						{p2.stats.map((s) => {
-							const statName = s.stat.name;
-							const statValue = s.base_stat;
+				<div className="column col-2 vs">
+					<img src={require("../assets/images/vs-min.png")} alt="vs img" />
+				</div>
 
-							return (
-								<div
-									className="modal__content__stats__container"
-									key={makeid()}
-								>
-									<small className="modal__content__stats__container__stat">
-										{statName}:&nbsp;
-									</small>
-									<small className="modal__content__stats__container__value">
-										{statValue}
-									</small>
-								</div>
-							);
-						})}
-					</div>
+				<div className="column col-5">
+					<PokemonData pokemonObj={pokemon2} />
 				</div>
 			</div>
 		</div>
---
+++ src/components/Pokemon.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeid } from "../functions/functions";
+const PokemonData = ({ pokemonObj }) => {
+	const p = pokemonObj;
+	return (
+		<div className={`pokemon-single ${p.types[0].type.name}`}>
+			<h4 className="pokemon-single__name">{p.name}</h4>
+			<img
+				className="pokemon-single__sprite"
+				src={
+					p.sprites.other.dream_world.front_default !== null
+						? p.sprites.other.dream_world.front_default
+						: p.sprites.front_default
+				}
+				alt={p.name}
+			/>
+			<p className="pokemon-single__stat-title">Base Stats</p>
+			{p.stats.map((s) => {
+				const statName = s.stat.name;
+				const statValue = s.base_stat;
+
+				return (
+					<div className="pokemon-single__stats__container" key={makeid()}>
+						<small className="pokemon-single__stats__stat">
+							{statName}:&nbsp;
+						</small>
+						<small className="pokemon-single__stats__value">{statValue}</small>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default PokemonData;
